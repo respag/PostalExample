a8f012479a78064248eb2b5d55d5f7e30d5e4d09
respag <respag@hotmail.com>
2014-09-10 14:31:11 -0500

    Primera Version

---
+++ Postal/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Postal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Postal_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            dynamic email = new Email("Example");
+            email.To = "respag@hotmail.com, rspagnuolo@ultimus.com, panamap@outlook.com";
+            email.FunnyLink = "http://blog.respag.net";//DB.GetRandomLolcatLink();
+            email.Send();
+            return View();
+        }
+    }
+}
---
+++ Postal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Postal_Example.MvcApplication" Language="C#" %>
